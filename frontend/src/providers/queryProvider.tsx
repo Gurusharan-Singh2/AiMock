1c7f86c120fc20f8dd51ae5777a62301f952b818
Gurusharan Singh <gurusharansingh2222@gmail.com>
2026-01-20 12:17:50 +0530

    20-01-2026

---
+++ frontend/src/providers/queryProvider.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { QueryClient, QueryClientProvider, useQueryClient } from "@tanstack/react-query"
+import React from "react"
+
+let browserQueryClient:QueryClient | undefined=undefined;
+
+
+
+function makeQueryClient(){
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 60, 
+        retry: 4,
+      },
+    },
+  });
+
+}
+
+function getQueryClient(){
+  if(typeof window==='undefined'){
+ return makeQueryClient();
+  }else {
+    if(!browserQueryClient){
+      browserQueryClient=makeQueryClient();
+
+    }
+    return browserQueryClient;
+  }
+}
+
+const queryClient=getQueryClient();
+export const QueryProvider=({children}:{children:React.ReactNode})=>{
+
+return (
+    <QueryClientProvider client={queryClient}>
+{children}
+        </QueryClientProvider>
+)     
+
+}
