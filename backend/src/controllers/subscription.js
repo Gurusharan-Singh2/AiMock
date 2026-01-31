13980951bfe86ea30c3034a6ed7c7a1b49f7933a
AniketKaushik19 <aniketsskaushik@gmail.com>
2026-01-31 18:26:28 +0530

    subscription plans

--- backend/src/controllers/subscription.js
+++ backend/src/controllers/subscription.js
@@ -1,6 +1,6 @@
 import crypto from 'crypto'
 import Razorpay from 'razorpay';
-
+import db from "../config/db.js";
 export const Subscription = async (req, res) => {
     console.log("subscription")
     try {
@@ -17,12 +17,14 @@ export const Subscription = async (req, res) => {
 
     const order = await razorpay.orders.create(options);
     res.json(order);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  } catch (error) {
+     console.error(error);
+        res.status(500).json({ message: "Internal server error" });
   }
 }
 
 export const SubscriptionVerify = async (req, res) => {
+  try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
 
   const hmac = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
@@ -33,5 +35,86 @@ export const SubscriptionVerify = async (req, res) => {
     res.json({ status: "success" });
   } else {
     res.status(400).json({ status: "failure" });
+  }
+    
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export const createSubscriptionPlan=async(req,res)=>{
+   try {
+      const {name , features , price}=req.body
+        if (!name || !features || !price) {
+      return res.status(400).json({ message: "Name , features and price are required" });
+    }
+      const plan = await db("subscriptions").insert({
+        name ,
+        features, 
+        price,
+        isActive:1,
+    });
+
+    if (!plan) {
+      return res.status(400).json({
+        message: "Failed to create , please try again",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Created Successfully",
+    });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+} 
+
+export const getSubscriptionPlan=async(req,res)=>{
+  try {
+    const plans = await db("subscriptions").select("*")
+
+    if (!plans) {
+      return res.status(404).json({
+        message: "Plans not found"
+      });
+    }
+    res.status(200).json({
+      message: " Plan get successfully",
+      plans
+    });
+
+  } catch (error) {
+     console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export const deleteSubscriptionPlan=async(req,res)=>{
+  try {
+     const {subscription_id}=req.params
+     console.log(subscription_id);
+     
+ const plans = await db("subscriptions")
+  .where({ subscription_id })
+  .del();
+
+
+    if (!plans) {
+      return res.status(404).json({
+        message: "Plans not deleted"
+      });
+    }
+    res.status(200).json({
+      message: " Plan deleted successfully",
+      plans
+    });
+
+
+  } catch (error) {
+     console.error(error);
+        res.status(500).json({ message: "Internal server error" });
   }
 }
--- backend/src/routes/subscription.js
+++ backend/src/routes/subscription.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { Subscription , SubscriptionVerify } from '../controllers/subscription.js'
+import { Subscription , SubscriptionVerify ,createSubscriptionPlan ,deleteSubscriptionPlan,getSubscriptionPlan } from '../controllers/subscription.js'
 
 const router =express.Router()
 
 router.post("/create" , Subscription )
 router.post("/verify-payment" , SubscriptionVerify )
-
+router.post("/createPlan" , createSubscriptionPlan )
+router.get("/getPlan" , getSubscriptionPlan)
+router.delete("/deletePlan/:subscription_id",deleteSubscriptionPlan)
 export default router
